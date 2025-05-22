de2600188d5e3a5a3d165a42ea9d91b4e2afa91b
YasinDehfuli <ydehfuli@gmail.com>
2025-05-22 16:39:24 +0330

    chore(component): learned how to used component inside of app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterModule} from "@angular/router";
+import {HeaderComponent} from "./components/header.component";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: 'components/header.component.html',
-  styleUrl: 'components/header.component.scss'
+  imports: [RouterModule,HeaderComponent],
+  selector: 'app-routes',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
-export class HeaderComponent {
+export class AppComponent {
   title = 'Angular-Course';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import {HeaderComponent} from "./app/components/header.component";
+import {AppComponent} from "./app/app.component";
 
-bootstrapApplication(HeaderComponent, appConfig)
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
