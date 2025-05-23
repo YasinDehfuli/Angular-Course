28b5bddec90dd78573c0a65d97bf3bf0abc6a1b9
YasinDehfuli <ydehfuli@gmail.com>
2025-05-23 12:58:39 +0330

    chore(component): learned how to generate components in angular using (ng g c components/header)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {RouterModule} from "@angular/router";
-import {HeaderComponent} from "./components/header.component";
+import {HeaderComponent} from "./components/header/header.component";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @Component({
-  imports: [RouterModule,HeaderComponent],
+  imports: [RouterModule,HeaderComponent, FooterComponent],
   selector: 'app-routes',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  imports: [],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  title = 'Angular-Course';
+
 }
