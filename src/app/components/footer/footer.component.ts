d76aeee60811502ef47025b9189bacba572a2916
YasinDehfuli <ydehfuli@gmail.com>
2025-05-24 23:47:42 +0330

    chore(angular): learned how to data binding & event binding

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,21 @@
 import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+    selector: 'app-footer',
+    imports: [],
+    templateUrl: './footer.component.html',
+    styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-  @Input({required: true})
-  public name!: string;
+    @Input({required: true})
+    public name!: string;
+
+    title = 'Hello World'
+    description = 'This is a footer'
+    uniqueClass = 'classColor'
+
+    public onCLick(event:Event, value:string) {
+        event.preventDefault();
+        this.uniqueClass = value
+    }
 }
