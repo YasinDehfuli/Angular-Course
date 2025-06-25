f24bc380784a2eeb1eb9155c18c82693de3d0765
Yasin <ydehfuli@gmail.com>
2025-06-25 13:31:23 +0330

    style(app): fixed app component imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,9 @@ import {Component} from '@angular/core';
 import {RouterModule} from "@angular/router";
 import {TodosComponent} from "./components/todo/todos/todos.component";
 import {NgIfComponent} from "./components/directives-learn/ng-if/ng-if.component";
-import {TestDirective} from "./test.directive";
 
 @Component({
-    imports: [RouterModule, TodosComponent, NgIfComponent, TestDirective],
+    imports: [RouterModule, TodosComponent, NgIfComponent],
     selector: 'app-routes',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
